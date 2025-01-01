d1380cba1c8f457d1126b22e0ad027c0b979dab0
Raphaël de Courville <groupes.raphael@gmail.com>
2025-01-01 20:53:55 +0100

    Add callsign

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
+
+let config = {
+  width: 320,
+  height: 256,
+  callsign: "SSTVXX",
+  xPos: 160,
+  yPos: 40,
+  fontSize: 64,
+};
+
 function setup() {
-  createCanvas(320, 256);
+  createCanvas(config.width, config.height);
   createUserInterface();
   pixelDensity(1);
 }
@@ -12,8 +22,28 @@ function draw() {
     ellipse(random(width), random(height), random(10, 100));
   }
 
-  // Set canvasData to the current canvas image
+  drawCallsign(config.callsign, config.xPos, config.yPos, config.fontSize);
+
+  // Set global canvasData for encoding
   canvasData = drawingContext.getImageData(0, 0, width, height);
-  
-  noLoop();
+
+  noLoop(); // Stop draw loop after one iteration
+}
+
+function drawCallsign(callsign, x = width/2, y = 40, size = 64) {
+  textSize(size);
+  fill(0);
+  stroke(255);
+  strokeWeight(5);
+  textAlign(CENTER, CENTER);
+  text(callsign, x, y); // Corrected variable name
+}
+
+function keyPressed() {
+  let k = key.toLowerCase();
+  switch (k) {
+    case "r":
+      redraw(); // Resume draw loop on mouse click
+      break;
+  }
 }
